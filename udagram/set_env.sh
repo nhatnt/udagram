cad906b99f6cc80e342f1b102c4970455e73991a
nhatnt <ngotannhat@gmail.com>
2023-09-28 10:20:22 +0700

    update authen

---
+++ udagram/set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_HOST=database-test.c6pqepgacaoq.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export AWS_ACCESS_KEY_ID=ASIAQNSAS2LVDITX5QNE
+export AWS_SECRET_ACCESS_KEY=7ZjpE5UlWsPFID1I/BaPLk84doPlF8McRf5VJDgC
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8080
